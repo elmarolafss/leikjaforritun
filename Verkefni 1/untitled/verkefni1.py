5eca73495a89dc9378bfb9b32f6b20ac8b882ef2
elmarolafss <elmarol250@nemi.tskoli.is>
2018-02-14 12:10:06 +0000

    verk190%

--- Verkefni 1/untitled/verkefni1.py
+++ Verkefni 1/untitled/verkefni1.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import sys
+from pygame import *
 pygame.init()
 
 
@@ -266,6 +268,162 @@ def the_collector():
         screen.blit(score, (screen_width / 2 - (score_rect[2] / 3), 80))
         pygame.display.update()
         clock.tick(FPS)
+"""
+Safnari endar
+"""
+"""
+Pong byrjar
+"""
+
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+ball_pos = [0,0]
+ball_vel = [0,0]
+paddle1_vel = 0
+paddle2_vel = 0
+l_score = 0
+r_score = 0
+
+window = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+
+def ball(right):
+    global ball_pos,ball_vel
+    ball_pos = [screen_width//2,screen_height//2]
+    horizontal = random.randrange(2,4)
+    vertical = random.randrange(2,4)
+
+    if right == False:
+        horizontal = - horizontal
+    ball_vel = [horizontal,-vertical]
+
+def init():
+    global paddle1_pos,paddle2_pos,paddle1_vel,paddle2_vel,l_score,r_score
+    global score1,score2
+    paddle1_pos = [HALF_PAD_WIDTH -1,screen_height//2]
+    paddle2_pos = [screen_width +1 - HALF_PAD_WIDTH,screen_height//2]
+    l_score = 0
+    r_score = 0
+    if random.randrange(0,2) == 0:
+        ball(True)
+    else:
+        ball(False)
+
+def draw(canvas):
+    global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score
+
+    canvas.fill(black)
+    pygame.draw.line(canvas, white, [screen_width // 2, 0], [screen_width // 2, screen_height], 1)
+    pygame.draw.line(canvas, white, [PAD_WIDTH, 0], [PAD_WIDTH, screen_height], 1)
+    pygame.draw.line(canvas, white, [screen_width - PAD_WIDTH, 0], [screen_width - PAD_WIDTH, screen_height], 1)
+
+
+
+    if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < screen_height - HALF_PAD_HEIGHT:
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == screen_height - HALF_PAD_HEIGHT and paddle1_vel < 0:
+        paddle1_pos[1] += paddle1_vel
+
+    if paddle2_pos[1] > HALF_PAD_HEIGHT and paddle2_pos[1] < screen_height - HALF_PAD_HEIGHT:
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == HALF_PAD_HEIGHT and paddle2_vel > 0:
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == screen_height - HALF_PAD_HEIGHT and paddle2_vel < 0:
+        paddle2_pos[1] += paddle2_vel
+
+
+    ball_pos[0] += int(ball_vel[0])
+    ball_pos[1] += int(ball_vel[1])
+
+    pygame.draw.circle(canvas, white, ball_pos, 20, 0)
+    pygame.draw.polygon(canvas, white, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT],
+                                        [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT],
+                                        [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT],
+                                        [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.polygon(canvas, white, [[paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT],
+                                        [paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT],
+                                        [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT],
+                                        [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT]], 0)
+
+
+    if int(ball_pos[1]) <= BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+    if int(ball_pos[1]) >= screen_height + 1 - BALL_RADIUS:
+        ball_vel[1] = -ball_vel[1]
+
+
+    if int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT, paddle1_pos[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
+        r_score += 1
+        ball(True)
+
+    if int(ball_pos[0]) >= screen_width + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) in range(
+            paddle2_pos[1] - HALF_PAD_HEIGHT, paddle2_pos[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball_pos[0]) >= screen_width + 1 - BALL_RADIUS - PAD_WIDTH:
+        l_score += 1
+        ball(False)
+
+
+    myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
+    label1 = myfont1.render("Score " + str(l_score), 1, (255, 255, 0))
+    canvas.blit(label1, (50, 20))
+
+    myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
+    label2 = myfont2.render("Score " + str(r_score), 1, (255, 255, 0))
+    canvas.blit(label2, (470, 20))
+
+
+def keydown(event):
+    global paddle1_vel, paddle2_vel
+
+    if event.key == K_UP:
+        paddle2_vel = -8
+    elif event.key == K_DOWN:
+        paddle2_vel = 8
+    elif event.key == K_w:
+        paddle1_vel = -8
+    elif event.key == K_s:
+        paddle1_vel = 8
+
+def keyup(event):
+    global paddle1_vel, paddle2_vel
+
+    if event.key in (K_w, K_s):
+        paddle1_vel = 0
+    elif event.key in (K_UP, K_DOWN):
+        paddle2_vel = 0
+
+def pong():
+
+    init()
+    while True:
+
+        draw(window)
+        for event in pygame.event.get():
+
+            if event.type == KEYDOWN:
+                keydown(event)
+            elif event.type == KEYUP:
+                keyup(event)
+            elif event.type == QUIT:
+                main_menu()
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+
+
 def main_menu():
     menu = True
     selected = "tening"
@@ -291,7 +449,7 @@ def main_menu():
                     if selected == "safn":
                         safnari("Reach 20 points!")
                     if selected == "pong":
-                        print("pong")
+                        pong()
                     if selected == "quit":
                         pygame.quit()
                         quit()
